d5845e1b9f1d6a90415176b0696ff0e3e149b8e0
Prathamesh Bhagat <bhagatprathamesh49@gmail.com>
2026-04-05 11:36:12 +0530

    Add files via upload

---
+++ day7.py
@@ -0,0 +1,12 @@
+marks = int(input("Enter your marks: "))
+
+if marks >= 90:
+    print("Congratulations! You are an A-Grade! ")
+elif marks >= 80:
+    print("Nice! you've got a B-grade!")
+elif marks >= 70:
+    print("You've got C (I know you can do better!)")
+elif marks >= 60:
+    print("It's a D!. You better start STUDYING!")
+else:
+    print("You have Failed the test.")
